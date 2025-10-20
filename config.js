e0c4383b1cce3dd9284d7084cde6e8d5a96ee63e
Eleen <eleen.och@gmail.com>
2025-10-21 00:54:30 +0800

    update with CORS headers

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  API_BASE: "https://script.google.com/macros/s/AKfycbzEsBPBgy2mdj2ixB8Kq0whyjU3wMT60DEYr0-HbwlvhNUTS6JIFgL_sXELNtnRCyIu/exec"
+  API_BASE: "https://script.google.com/macros/s/AKfycbzYg3Jtiuz3ts01Yne1q_l-Er1c5MP_gE_HRJmVdtJxgzdrmwN6kZ1D1yrkV3Jh1wYL/exec"
 };
